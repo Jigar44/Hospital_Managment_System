5ce9ea2af06fb576147df11e33244c0c46ef8cf5
vikashsing21 <vikashsing21@gmail.com>
2020-06-29 14:20:42 +0530

    Config file cached

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-class Config(object):
-    SECRET_KEY=os.urandom(24).hex()
-    SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='', server='localhost:3306', database='hms')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
